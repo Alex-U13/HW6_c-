2654d7f3ca3089f2bc372a3e740f2158a17efdfa
Alex <uvarovaas@ya.ru>
2022-10-02 17:05:22 +0700

    уточнение в 43

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)  //!!! Верно ли решение в качестве примера? 
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)  //
 
 Console.Clear();
 Console.Write("Введите b1: ");
@@ -35,7 +35,7 @@ double b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
-double x = (b1-b2)/(k1-k2);
+double x = (b1-b2)/(k1-k2)*(-1);
 double y = k2 * x + b2;
 
 Console.Write($"Точка пересечения двух прямых ({x};{y})");
